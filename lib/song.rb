c31e9ab96014b99ba3901476ab5d3023e837b255
Nicole Dow <nikki.dow71@gmail.com>
2019-07-22 21:23:58 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+  attr_reader :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+end
